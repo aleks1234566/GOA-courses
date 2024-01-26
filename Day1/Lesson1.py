8d8c23543df0eed48c2e110e38bf8c915bc58c19
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-01-26 21:07:45 +0400

    first commit

---
+++ Day1/Lesson1.py
@@ -0,0 +1,6 @@
+ 
+name="Aleqsandre "
+surname="Ghomidze "
+age=14
+height=173
+print("hello my name is " + name + surname + "I am " + str(age) + " years old  I am " + str(height) + " cm in height" ) 
