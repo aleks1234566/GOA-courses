deb3d5402fc3204d33bbc34eb5837f0dd25da9ff
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-03-08 14:03:17 +0400

    tenth commit

---
+++ Day10/classwork/classwork10.py
@@ -0,0 +1,16 @@
+for i in range(1,31):
+    print(i)
+
+for i in range(1, 11):
+    print(i * i)
+
+for i in range(1,11):
+    num= 0
+    print(num + i)
+
+for i in range(1,21):
+    print(i / 2)
+
+for i in range(1,10):
+    num= 1
+    print(i * num)
---
+++ Day10/homework/homework01.py
@@ -0,0 +1,34 @@
+for i in range(0, 21):
+    print(i)
+
+num0= int(input())
+price= int(input())
+
+for i in range(num0,price):
+    print(i)
+
+
+for i in range(49,101):
+    print(i)
+
+
+for i in range(101,49):
+    print(i)
+
+
+ 
+
+for i in range(-1,50):
+    print(i + 1)
+
+
+num= int(input())
+
+for i in range(0, num):
+    print(i)
+
+age= int(input())
+second_age= int(input()) 
+
+for i in range(age, second_age):
+    print(i)
