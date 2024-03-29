d429530da46bc2e17cfd615a7b4b7aacd8c8d365
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-03-29 17:57:09 +0400

    seventheenth commit

---
+++ Day16/homework/homework01.py
@@ -0,0 +1,18 @@
+names = ["Aleqsandre", "Zura", "Nino", "Nikolozi", "Gabrieli"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+
+surname = "Ghomidze"
+print(surname[0] + surname[1] + surname[2] + surname[3] + surname[4] + surname[5] + surname[6] + surname[7])
+
+
+user_name = input("Enter your Username: ")
+user_adress = input("Emter your adress: ")
+user_surname = input("Enter your surname")
+user_age = int(input("Enter your age: "))
+user_info = [user_name, user_adress, user_surname, user_age]
+print(user_info)
