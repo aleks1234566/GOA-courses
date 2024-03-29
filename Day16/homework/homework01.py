a133596d25cf834de24097195919f9eee8dc8fdf
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-03-29 19:33:04 +0400

    Eighteenth commit

--- Day16/homework/homework01.py
+++ Day16/homework/homework01.py
@@ -6,6 +6,21 @@ print(names[3])
 print(names[4])
 
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers[0])
+
+
+numbers1 = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+numbers1[0] = 1
+numbers1[2] = 1
+numbers1[4] = 1
+numbers1[6] = 1
+numbers1[8] = 1
+numbers1[10] = 1
+print(numbers1)
+
+
+print(numbers[9])
 surname = "Ghomidze"
 print(surname[0] + surname[1] + surname[2] + surname[3] + surname[4] + surname[5] + surname[6] + surname[7])
 
