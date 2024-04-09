3f27ec4e71337adc298957a92103d3bf3e3f589a
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-04-09 16:17:51 +0400

    Twentysecond commit

---
+++ Day19/homework/homework01.py
@@ -0,0 +1,6 @@
+#1) შექმენით პროგრამა სადაც გექნებათ სახელების სია. შემდეგ მომხმარებელს შემოატანინეთ რიცხვი და სიიდან მაგ ინდექსზე მყოფი ელემენტი გამოიტანეთ.
+names = ["Luka", "Gio", "Ioane", "Irakli", "Lazare"]
+index_num = int(input("Please enter your index number: "))
+print(names[index_num])
+
+ 
