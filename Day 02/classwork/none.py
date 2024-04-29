d16779af4788f5cff6a065eb4c93a6fbda871c61
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-04-29 14:22:43 +0400

    Thirtyfirst commit

---
+++ Day 02/classwork/none.py
@@ -0,0 +1,5 @@
+days = ['Monday', 'Tuesday', 'Wednesday']
+tasks = ['Gym', 'Homework', 'Pool']
+for day in days:
+ for task in tasks:
+    print(day, task)
--- Day 24/homework/1lesson24.py
+++ Day 24/homework/1lesson24.py
@@ -1,5 +1,5 @@
-#1)შექმენით 3 ფუნქცია თქვენი ფანტაზიით, გამოიყენეთ პარამეტრები და return keyword. (ძალიან მარტივები არ გინდათ, რაც უფრო რთულს გააკეთებთ თქვენი დონისთვის მით უფრო კარგი იქნებაA
-#A) 
+#შექმენით 3 ფუნქცია თქვენი ფანტაზიით, გამოიყენეთ პარამეტრები და return keyword. (ძალიან მარტივები არ გინდათ, რაც უფრო რთულს გააკეთებთ თქვენი დონისთვის მით უფრო კარგი იქნებაA
+#1) 
 def Log_in(password, num ):
       while password !=  "Goa":
             if num == 0:
@@ -13,4 +13,19 @@ Log_in(input("Please Enter your password: "), 2)
 
 
 
-#B)
+#2)
+def game_record() :
+   records = [0]
+   num = 0 
+   for i in records:
+      new_record = int(input("Please enter your record: "))
+      num += new_record
+       
+      if num > 0 : 
+             
+            print("Congratulations your new record is ", new_record)
+            records.append(new_record)
+            print(records)
+      else: 
+          return ("Try again")
+game_record()
